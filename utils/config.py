272093a574f7134a94dda106d9f5f1e3464609b6
Erik Garcia Oyono <eg424@ic.ac.uk>
2024-12-13 23:09:45 +0000

    Create config.py

---
+++ utils/config.py
@@ -0,0 +1,12 @@
+config = {
+    "buffer_capacity": 50000,
+    "batch_size": 64,
+    "std_dev": 0.2,
+    "critic_lr": 0.002,
+    "actor_lr": 0.001,
+    "total_episodes": 100,
+    "gamma": 0.99,
+    "tau": 0.005,
+    "num_steps": 100,
+    "model_dir": "./saved_models",
+}
